8c48777dc4496ceb173259b07d8794200c77b94a
KrystekYTpv <93543150+KrystekYTpv@users.noreply.github.com>
2022-08-20 13:31:21 +0200

    Update hdfvlsifvldifdsbjsf_LO.lua

--- hdfvlsifvldifdsbjsf_LO.lua
+++ hdfvlsifvldifdsbjsf_LO.lua
@@ -1,42 +1,55 @@
 
 if game.PlaceId == 6284583030 then
     print("Finded Game: Pet Simulator X")
+    getgenv().CheckGame = "Pet Simulator X"
     loadstring(game:HttpGet("https://raw.githubusercontent.com/KSDKrystek/KSD/main/siufvosz8fsttv89sy8_PXLOD.lua", true))()
 elseif game.PlaceId == 10321372166 then
     print("Finded Game: Hardcore Pet Simulator X")
+    getgenv().CheckGame = "Hardcore Pet Simulator X"
     loadstring(game:HttpGet("https://raw.githubusercontent.com/KSDKrystek/KSD/main/aisfaoisfasfponjpasonfa_PXH.lua", true))()
 elseif game.PlaceId == 7722306047 then
     print("Finded Game: Pet Simulator X Trade Plaza")
+    getgenv().CheckGame = "Pet Simulator X Trade Plaza"
     loadstring(game:HttpGet("https://raw.githubusercontent.com/KSDKrystek/KSD/main/siufvosz8fsttv89sy8_PXLOD.lua", true))()
 elseif game.PlaceId == 7336128600 then
     print("Finded Game: Pet Legends")
+    getgenv().CheckGame = "Pet Legends"
     loadstring(game:HttpGet("https://github.com/KSDKrystek/KSD/blob/main/hisjdsbosdngisdfgn_PL.lua", true))()
 elseif game.PlaceId == 7244314500 then
     print("Finded Game: Fightman Simulator")
+    getgenv().CheckGame = "Fightman Simulator"
     loadstring(game:HttpGet("https://raw.githubusercontent.com/KSDKrystek/KSD/main/jbndflksujbfskjdfvskdufs_FS.lua", true))()
 elseif game.PlaceId == 2512643572 then
     print("Finded Game: Bubble Gum Simulator")
+    getgenv().CheckGame = "Bubble Gum Simulator"
     loadstring(game:HttpGet("https://raw.githubusercontent.com/KSDKrystek/KSD/main/sjdbfslikjhgsdkfjgbdf_BG.lua", true))()
 elseif game.PlaceId == 8540346411 then
     print("Finded Game: Rebirth Champions X")
+    getgenv().CheckGame = "Rebirth Champions X"
     loadstring(game:HttpGet("https://raw.githubusercontent.com/KSDKrystek/KSD/main/knhjghdiugodxilujb_RCX.lua", true))()
 elseif game.PlaceId == 8303902695 then
     print("Finded Game: Pet Posse")
+    getgenv().CheckGame = "Pet Posse"
     loadstring(game:HttpGet("https://raw.githubusercontent.com/KSDKrystek/KSD/main/sojupgfa9tgs9du8fg_PPOS.lua", true))()
 elseif game.PlaceId == 9170761016 then
     print("Finded Game: Magic Clicker")
+    getgenv().CheckGame = "Magic Clicker"
     loadstring(game:HttpGet("https://raw.githubusercontent.com/KSDKrystek/KSD/main/sojgsbhp9ug9s8nypvr9sdty_MCL.lua", true))()
 elseif game.PlaceId == 606849621 then
     print("Finded Game: Jailbreak")
+    getgenv().CheckGame = "Jailbreak"
     loadstring(game:HttpGet("https://raw.githubusercontent.com/KSDKrystek/KSD/main/jdbsdjgosdgbsnd_JB.lua", true))()
 elseif game.PlaceId == 4787629450 then
     print("Finded Game: Booga Booga Classic")
+    getgenv().CheckGame = "Booga Booga Classic"
     loadstring(game:HttpGet("https://raw.githubusercontent.com/KSDKrystek/KSD/main/ajf8uyivsidfsdofhi_BBC.lua", true))()
 elseif game.PlaceId == 7606302010 then
     print("Finded Game: YoYo Simulator")
+    getgenv().CheckGame = "YoYo Simulator"
     loadstring(game:HttpGet("https://raw.githubusercontent.com/KSDKrystek/KSD/main/asiupiasufgaofhoasif_YOS.lua", true))()
 elseif game.PlaceId == 7305309231 then
     print("Finded Game: Taxi Boss")
+    getgenv().CheckGame = "Taxi Boss"
     loadstring(game:HttpGet("https://raw.githubusercontent.com/KSDKrystek/KSD/main/aslhvaslifasfasfasjkfbasjk_TAXB.lua", true))()
 else
     print("Game not supported")
